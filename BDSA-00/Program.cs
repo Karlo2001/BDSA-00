9450c58a0276d02e3d0525c9f78333c09bb6a58a
Karl Michelsen <43517053+Karlo2001@users.noreply.github.com>
2021-09-07 17:09:54 +0200

    Hello world and test

---
+++ BDSA-00/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDSA_00
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
