0911b91a3549fec6926d714845d1aeeb7d7480ed
Karlo2001 <43517053+Karlo2001@users.noreply.github.com>
2021-09-08 15:56:22 +0200

    Added IsLeapYear with tests (#2)
    Added IsLeapYear with tests

--- BDSA-00.Tests/UnitTest1.cs
+++ BDSA-00.Tests/UnitTest1.cs
@@ -18,4 +18,20 @@ namespace BDSA_00.Tests
             Assert.Equal("Hello World!", result);
         }
     }
+
+    public class Leap_Year_Tests
+    {
+        [Theory]
+        [InlineData(4, true)]
+        [InlineData(100, false)]
+        [InlineData(200, false)]
+        [InlineData(204, true)]
+        [InlineData(400, true)]
+        [InlineData(1800, false)]
+        [InlineData(1600, true)]
+        public void Check_Leap_Year(int year, bool expected)
+        {
+            Assert.Equal(expected, Program.IsLeapYear(year));
+        }
+    }
 }
--- BDSA-00/Program.cs
+++ BDSA-00/Program.cs
@@ -8,5 +8,14 @@ namespace BDSA_00
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static bool IsLeapYear(int year)
+        {
+            if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
