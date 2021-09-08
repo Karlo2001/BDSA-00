70156ce9af90a5e40a98fcdac06ec591a02e5531
Karlo2001 <43517053+Karlo2001@users.noreply.github.com>
2021-09-08 16:33:21 +0200

    Added user input for IsLeapYear with tests (#3)
    Added user input for IsLeapYear with tests

--- BDSA-00.Tests/UnitTest1.cs
+++ BDSA-00.Tests/UnitTest1.cs
@@ -4,21 +4,6 @@ using System.IO;
 
 namespace BDSA_00.Tests
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-
-            Program.Main(new string[0]);
-            var result = writer.GetStringBuilder().ToString().Trim();
-
-            Assert.Equal("Hello World!", result);
-        }
-    }
-
     public class Leap_Year_Tests
     {
         [Theory]
@@ -32,6 +17,25 @@ namespace BDSA_00.Tests
         public void Check_Leap_Year(int year, bool expected)
         {
             Assert.Equal(expected, Program.IsLeapYear(year));
+        }
+        [Theory]
+        [InlineData("4", "yay")]
+        [InlineData("100", "nay")]
+        [InlineData("200", "nay")]
+        [InlineData("204", "yay")]
+        [InlineData("400", "yay")]
+        [InlineData("1800", "nay")]
+        [InlineData("1600", "yay")]
+        public void Check_Leap_Year_With_User_Input(string input, string expected)
+        {
+            Console.SetIn(new StringReader(input));
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.Main(new string[0]);
+            var result = writer.GetStringBuilder().ToString().Trim();
+
+            Assert.Equal(expected, result.Substring(result.Length - 3));
         }
     }
 }
