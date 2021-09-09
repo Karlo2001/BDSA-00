ec4d88287a4651c8d1611f0c49777c9708edc181
Karlo2001 <43517053+Karlo2001@users.noreply.github.com>
2021-09-09 08:09:03 +0200

    Implemented error handling with tests (#4)

--- BDSA-00/Program.cs
+++ BDSA-00/Program.cs
@@ -4,17 +4,46 @@ namespace BDSA_00
 {
     public class Program
     {
+        public const string ERROR_INPUT_YEAR_BELOW_1582 = "You can only test years from 1582";
+        public const string ERROR_INPUT_CANT_CONVERT = "The input is not a valid number";
+        public const string ERROR_INPUT_OVERFLOW = "The input was either too large or too small";
         public static void Main(string[] args)
         {
             Console.Write("Insert a year to see if it is a leap year: ");
             string input = Console.ReadLine();
-            int year = Convert.ToInt32(input);
-
-            if(IsLeapYear(year))
+            int year = 0;
+            try
             {
-                Console.WriteLine("yay");
-            } else {
-                Console.WriteLine("nay");
+                year = Convert.ToInt32(input);
+            } 
+            catch (FormatException e)
+            {
+                Console.WriteLine(ERROR_INPUT_CANT_CONVERT);
+                return;
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(ERROR_INPUT_OVERFLOW);
+                return;
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.ToString());
+                return;
+            }
+
+            if(year >= 1582) 
+            {
+                if(IsLeapYear(year))
+                {
+                    Console.WriteLine("yay");
+                } else {
+                    Console.WriteLine("nay");
+                }
+            }
+            else
+            {
+                Console.WriteLine(ERROR_INPUT_YEAR_BELOW_1582);
             }
         }
 
