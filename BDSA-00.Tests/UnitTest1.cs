fd1659b3cc2f78969be7747a04ca2ff0026361e7
Karl Michelsen <43517053+Karlo2001@users.noreply.github.com>
2021-09-09 08:07:58 +0200

    Implemented error handling with tests

--- BDSA-00.Tests/UnitTest1.cs
+++ BDSA-00.Tests/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace BDSA_00.Tests
 {
     public class Leap_Year_Tests
     {
+
         [Theory]
         [InlineData(4, true)]
         [InlineData(100, false)]
@@ -19,11 +20,10 @@ namespace BDSA_00.Tests
             Assert.Equal(expected, Program.IsLeapYear(year));
         }
         [Theory]
-        [InlineData("4", "yay")]
-        [InlineData("100", "nay")]
-        [InlineData("200", "nay")]
-        [InlineData("204", "yay")]
-        [InlineData("400", "yay")]
+        [InlineData("2000", "yay")]
+        [InlineData("1584", "yay")]
+        [InlineData("2000", "yay")]
+        [InlineData("1582", "nay")]
         [InlineData("1800", "nay")]
         [InlineData("1600", "yay")]
         public void Check_Leap_Year_With_User_Input(string input, string expected)
@@ -36,6 +36,31 @@ namespace BDSA_00.Tests
             var result = writer.GetStringBuilder().ToString().Trim();
 
             Assert.Equal(expected, result.Substring(result.Length - 3));
+        }
+
+        [Theory]
+        [InlineData("4", Program.ERROR_INPUT_YEAR_BELOW_1582)]
+        [InlineData("100", Program.ERROR_INPUT_YEAR_BELOW_1582)]
+        [InlineData("string", Program.ERROR_INPUT_CANT_CONVERT)]
+        [InlineData("", Program.ERROR_INPUT_YEAR_BELOW_1582)]
+        [InlineData("0A1B2", Program.ERROR_INPUT_CANT_CONVERT)]
+        [InlineData("2147483648", Program.ERROR_INPUT_OVERFLOW)]
+        public void Check_Leap_Year_With_Faulty_User_Input(string input, string expected)
+        {
+            Console.SetIn(new StringReader(input));
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.Main(new string[0]);
+            var result = writer.GetStringBuilder().ToString().Trim();
+            char[] delims = new[] { '\r', '\n' };
+            string[] strings = result.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+            if(strings.Length == 1)
+            {
+                strings = strings[0].Split(':', StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Assert.Equal(expected, strings[strings.Length-1].Trim());
         }
     }
 }
